66b36e0d110f85eb590d6a8e54263fe8481d3232
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-02-27 00:43:39 +0400

    Design in progress

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const btn = document.querySelector(".btnSubmit");
 const todoContainer = document.querySelector(".items");
 const inputField = document.querySelector(".field");
-
+const btnDelete = document.querySelector(".delete");
+const container = [];
 btn.addEventListener('click', () => {
   const inputValue = document.querySelector(".field").value;
   if (inputValue === '') {
     console.log("Please add an item");
   }
-  todoContainer.innerHTML = inputValue;
-  inputField.value = "";
+  container.push(inputValue);
+
+  container.forEach((element) => {
+    todoContainer.innerHTML += element;
+    btnDelete.style.display = "block";
+    inputField.value = "";
+  });
 
 })
