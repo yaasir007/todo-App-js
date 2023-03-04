7eccef369aed5cb32228121aa1df008ea830ab32
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-03-04 23:52:58 +0400

    redoing logic

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const inputElement = document.querySelector(".fieldBox");
-const btnAddElement = document.querySelector(".btnSubmit");
+// const inputElement = document.querySelector(".fieldBox");
+// const btnAddElement = document.querySelector(".btnSubmit");
 
-const AllTodoItemElement = document.querySelector(".displayTodos");
-const btnDeleteElement = document.querySelector(".delete");
+// const AllTodoItemElement = document.querySelector(".displayTodos");
+// const btnDeleteElement = document.querySelector(".delete");
 
-AllTodoItemElement.style.display = "none";
+// AllTodoItemElement.style.display = "none";
 
-const alltoDos = [];
+// const alltoDos = [];
 
-btnAddElement.addEventListener("click", (e) => {
-  e.preventDefault();
-  let inputValue = inputElement.value;
+// btnAddElement.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   let inputValue = inputElement.value;
 
-  let createTodo = () => {
-    AllTodoItemElement.innerHTML += `
-    <div class="items">
-    <span class="todoContent">${inputValue}</span>
-    <button class="delete" onClick="deleteTodo(this)"><i class="fa-solid fa-trash"></i></button>
-    </div>
-    `;
+//   let createTodo = () => {
+//     AllTodoItemElement.innerHTML += `
+//     <div class="items">
+//     <span class="todoContent">${inputValue}</span>
+//     <button class="delete" onClick="deleteTodo(this)"><i class="fa-solid fa-trash"></i></button>
+//     </div>
+//     `;
 
-    AllTodoItemElement.style.display = "block";
+//     AllTodoItemElement.style.display = "block";
 
-  }
+//   }
 
-  if (inputValue !== "") {
-    alltoDos.push(inputValue);
-    createTodo();
-    inputValue = "";
-  } else {
-    alert("Please enter a valid todo")
-  }
+//   if (inputValue !== "") {
+//     alltoDos.push(inputValue);
+//     createTodo();
+//     inputValue = "";
+//   } else {
+//     alert("Please enter a valid todo")
+//   }
 
-});
+// });
